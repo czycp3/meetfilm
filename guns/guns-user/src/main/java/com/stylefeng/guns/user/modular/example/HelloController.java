a59c9fed5414dcd46760454b4d5c4cad9afa3fdf
DanIsADreamer <dansdream@163.com>
2019-06-04 22:08:01 +0800

    增加用户模块

---
+++ guns/guns-user/src/main/java/com/stylefeng/guns/user/modular/example/HelloController.java
@@ -0,0 +1,17 @@
+package com.stylefeng.guns.user.modular.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author czy
+ * @date 2019/6/4 14:41
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "ok";
+    }
+}
